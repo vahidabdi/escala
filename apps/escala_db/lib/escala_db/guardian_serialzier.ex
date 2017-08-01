4e692d4dbf2677a7fffadfccd12c26ae2513e7c7
Vahid Abdi <vahidabdinet@gmail.com>
2017-08-01 12:25:59 +0430

    Guardian Serializer

--- apps/escala_db/lib/escala_db/guardian_serialzier.ex
+++ apps/escala_db/lib/escala_db/guardian_serialzier.ex
@@ -1,16 +1,16 @@
-# defmodule EscalaDb.GuardianSerializer do
-#   @moduledoc """
-#   Guardian Serializer
-#   """
-#
-#   @behaviour Guardian.Serializer
-#
-#   alias EscalaDb.Accounts
-#   alias EscalaDb.Accounts.User
-#
-#   def for_token(%User{} = user), do: {:ok, "User:#{user.id}"}
-#   def for_token(_), do: {:error, "Unknown resource type"}
-#
-#   def from_token("User:" <> id), do: {:ok, Accounts.get_user(id)}
-#   def from_token(_), do: {:error, "Unknown resource type"}
-# end
+defmodule EscalaDb.GuardianSerializer do
+  @moduledoc """
+  Guardian Serializer
+  """
+
+  @behaviour Guardian.Serializer
+
+  alias EscalaDb.Accounts
+  alias EscalaDb.Accounts.User
+
+  def for_token(%User{} = user), do: {:ok, "User:#{user.id}"}
+  def for_token(_), do: {:error, "Unknown resource type"}
+
+  def from_token("User:" <> id), do: {:ok, Accounts.get_user(id)}
+  def from_token(_), do: {:error, "Unknown resource type"}
+end
